03d79ea67b35dd36a5f45bba7b719087fc8143ec
xiang.yu <xiang.yu@17daxue.com>
2018-07-02 16:53:53 +0800

    eslint alloy import

--- app/controller/news.js
+++ app/controller/news.js
@@ -1,11 +1,11 @@
-const Controller =require('egg').Controller;
+const Controller = require('egg').Controller;
 
-class NewsController extends Controller{
+class NewsController extends Controller {
     async list() {
-        const ctx = this.ctx
-        const page = ctx.query.page || 1
-        const newsList = await ctx.service.news.list(page)
-        await ctx.render('news/list.tpl',{list:newsList})
+        const ctx = this.ctx;
+        const page = ctx.query.page || 1;
+        const newsList = await ctx.service.news.list(page);
+        await ctx.render('news/list.tpl', { list: newsList });
     }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,11 @@
 exports.keys = 'xianggg';
+// mongo config
+exports.mongose = {
+    client:{
+        url:'mongodb://xianggg:!@#456@47.95.255.156/family',
+        option:{}
+    }
+}
 // 添加view配置
 exports.view ={
     defaultViewEngine:'nunjucks',
